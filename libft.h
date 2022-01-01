e50846e23f70390dd709bcca24c27a854b6c3b32
yabdelgh <yassine.abdou.1998@gmail.com>
2022-01-01 20:45:22 +0100

    ft_strlen 100% + libft.h + main.c

---
+++ libft.h
@@ -0,0 +1,6 @@
+#ifndef __LIBFT_H__
+#define __LIBFT_H__
+
+int ft_strlen(const char *str);
+
+#endif
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "libft.h"
+
+int main()
+{
+	char *str;
+
+	str = "i love seffa";
+	printf("%s\n",str);	
+	printf("%d\n",ft_strlen(str));
+
+	return (0);	
+}
