0f1d7b365fa243e1512eb044426e813cc1b3a681
yabdelgh <yassine.abdou.1998@gmail.com>
2022-01-02 23:12:56 +0100

    tests for ft_isalpha

--- libft.h
+++ libft.h
@@ -1,6 +1,7 @@
 #ifndef __LIBFT_H__
 #define __LIBFT_H__
 
+int ft_isalpha(int c);
 int ft_strlen(const char *str);
 
 #endif
--- main.c
+++ main.c
@@ -5,9 +5,18 @@ int main()
 {
 	char *str;
 
+	// ft_strlen
+	printf("\nft_strlen:\n");
 	str = "i love seffa";
 	printf("%s\n",str);	
-	printf("%d\n",ft_strlen(str));
+	printf("%d\n\n",ft_strlen(str));
 
+	// ft_isalpha
+	printf("ft_isalpha:\n");
+	printf("a: %d\n", ft_isalpha('a'));
+	printf("Q: %d\n", ft_isalpha('Q'));
+	printf("z: %d\n", ft_isalpha('z'));
+	printf("4: %d\n", ft_isalpha('4'));
+	
 	return (0);	
 }
